0658670fb100ad8426d320b7c8808503c58c47f7
Amy1740 <amina.hiel@gmail.com>
2023-06-15 12:53:52 -0700

    commit optional tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+* main - Main entry point description
+* Return: Always 1 (Success)
+*/
+int main(void)
+{
+char m[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+write(2, m, 59);
+return (1);
+}
+
