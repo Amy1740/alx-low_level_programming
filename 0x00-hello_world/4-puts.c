640b11158d725e68f7d186483b39988d3793431a
Amy1740 <amina.hiel@gmail.com>
2023-06-15 03:52:02 -0700

    commit 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main- Entry print
+*
+*Description: Print "Programming is like building o multilinguale puzzle
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts(""Programming is like building o multilinguale puzzle");
+return (0);
+}
