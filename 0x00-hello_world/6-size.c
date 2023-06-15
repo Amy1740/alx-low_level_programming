545b8bf5287f85acf054479e8b10af583f2b5cc9
Amy1740 <amina.hiel@gmail.com>
2023-06-15 06:01:04 -0700

    commit size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
 *main -Entry point
-*Description: prints "the size of data types"
+*Description: prints \"the size of data types\"
 *Return: Always 0 (Success)
 */
 int main(void)
 {
-printf("Size of a char:%lu byte(s)\n", 'unsigned long'sizeof(char));
-printf("Size of a int:%lu byte(s)\n", 'unsigned long'sizeof(int));
-printf("Size of a long int:%lu byte(s)\n", 'unsigned long'sizeof(long int));
-printf("Size of a long long:%lu byte(s)\n", 'unsigned long'sizeof(long long));
-printf("Size of a float:%lu byte(s)\n", 'unsigned long'sizeof(float));
+printf("Size of a char:%lu byte(s)\n", (unsigned long)sizeof(char));
+printf("Size of a int:%lu byte(s)\n", (unsigned long)sizeof(int));
+printf("Size of a long int:%lu byte(s)\n", (unsigned long)sizeof(long int));
+printf("Size of a long long:%lu byte(s)\n", (unsigned long)sizeof(long long));
+printf("Size of a float:%lu byte(s)\n", (unsigned long)sizeof(float));
 
 return (0);
 }
