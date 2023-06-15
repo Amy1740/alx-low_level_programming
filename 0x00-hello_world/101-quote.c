4adac8435f2e95a99ce69a8468bc2954d69c9dec
Amy1740 <amina.hiel@gmail.com>
2023-06-15 12:57:50 -0700

    commit optional tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
 */
 int main(void)
 {
-char m[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+char m[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, m, 59);
 return (1);
 }
