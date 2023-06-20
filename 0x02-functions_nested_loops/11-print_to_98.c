35f083f4016754255057607e17397ca8ebed165c
Amy1740 <amina.hiel@gmail.com>
2023-06-20 14:23:05 -0700

    commit 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,17 +8,26 @@
  * from n to 98, followed by a new line.
  * Return: Nothing
  */
-void print_to_98(int n)
-{
-	int i;
-
-	for (i = n; i <= 98; i++)
-	{
-		printf("%d", i);
-	if (i != 98)
-	{
-		printf(", ");
-	}
-	}
-printf("\n");
+void print_to_98(int n) {
+    int i;
+    
+    if (n <= 98) {
+        for (i = n; i <= 98; i++) {
+            printf("%d", i);
+            
+            if (i != 98) {
+                printf(", ");
+            }
+        }
+    } else {
+        for (i = n; i >= 98; i--) {
+            printf("%d", i);
+            
+            if (i != 98) {
+                printf(", ");
+            }
+        }
+    }
+    
+    printf("\n");
 }
